960a57a77ac9975c7a7670fa05d21f66ac85db5e
Nicoou <nf9600362@gmail.com>
2023-08-01 12:48:09 -0700

    delete create

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,8 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -14,11 +15,11 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, "w") as f:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
@@ -36,15 +37,30 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, "r") as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Removes 'obj' from  __objects if present. If 'obj' is None, the method does nothing"""
+        if obj is not None:
+            # Check if 'obj' is a valid object
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            # Generate a unique key for the object using its class name and 'id' attribute.
+            if key in self.__objects:
+                # If the key exists in the '__objects' dictionary
+                del self.__objects[key]
+            # Delete the corresponding object from the dictionary.
