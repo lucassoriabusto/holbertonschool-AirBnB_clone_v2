bc90072d8c26c675f5ffe1d18df980addaf1c025
Nicoou <nf9600362@gmail.com>
2023-08-03 15:37:12 -0700

    delete comment

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
---  los privilegios se aplicarán a todas las tablas y objetos dentro de la base de datos llamada "hbnb_dev_db"
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
 GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
