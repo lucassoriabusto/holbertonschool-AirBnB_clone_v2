aed99dd3cbe07fa4ae1cc2f45df8b739f035189c
Nicoou <nf9600362@gmail.com>
2023-08-03 15:36:21 -0700

    rev

--- models/city.py
+++ models/city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
--- models/place.py
+++ models/place.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-
 from models.base_model import BaseModel
-from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
     """ A place to stay """
-
     city_id = ""
     user_id = ""
     name = ""
     description = ""
-    number_rooms = ""
-    number_bathrooms = ""
-    max_guest = ""
-    price_by_night = ""
-    latitude = ""
-    longitude = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
-
-
-
+    
