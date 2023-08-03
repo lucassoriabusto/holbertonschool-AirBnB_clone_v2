579ac685efb253a60a836344ead624f594ebcd1e
Nicoou <nf9600362@gmail.com>
2023-08-03 13:25:50 -0700

    task 8

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -12,3 +12,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Places" cascade="all, delete-orphan", backref="user")
