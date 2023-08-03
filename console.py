655b2db62b8bafdfadb50ce469eb459cdfeea856
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-03 14:38:01 -0700

    do_create updated

--- console.py
+++ console.py
@@ -118,10 +118,23 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        list_arg = args.split()
+        class_name = list_arg[0]
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        new_instance = HBNBCommand.classes[class_name]()
+
+        for arg in list_arg[1:]:
+            key, value = arg.split("=")
+            if isinstance(value, str):
+                value = value.replace("\"", "")
+                value = value.replace("_", " ")
+            setattr(new_instance, key, value)
+
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -319,6 +332,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
