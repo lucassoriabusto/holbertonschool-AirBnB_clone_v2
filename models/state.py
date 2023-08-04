80429d08c11091bcb0cedbb559a701e5ed33b9e2
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-03 19:18:57 -0700

    state.py update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModeli, Base
+from sqlalchemy import Column, String, ForeignKey
+import os  # Importa el módulo os para acceder a variables de entorno
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    """ Si estás utilizando DBStorage, agrega la relación con la clase City """
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    else:
+        """ Si estás utilizando FileStorage,
+        agrega el getter attribute para las ciudades """
+        @property
+        def cities(self):
+            """ Atributo getter que devuelve la lista de instancias de City
+            con state_id igual al State.id actual """
+            from models import storage
+            city_instances = storage.all(City)
+            return [city for city in city_instances.values() if city.state_id == self.id]
