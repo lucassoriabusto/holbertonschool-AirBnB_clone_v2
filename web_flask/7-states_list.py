ac4625a7a2aba193308444ae7c76226c442268f5
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-18 20:24:06 -0700

    Task 08

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,16 +9,16 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
