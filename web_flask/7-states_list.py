2f993b6176cf5d0f42a7198b3348609774374309
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-19 10:33:40 -0700

    Task 08

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application. """
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
-
-    @app.teardown_appcontext
-    def teardown_db(exception):
-        storage.close()
 
 
 if __name__ == "__main__":
